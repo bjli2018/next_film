b7ddeccb0f39e12707a31ec3eaf27a85d6a1e460
dkq6686 <Bob.Li@varian.com>
2019-10-17 13:28:00 +0800

    project init

---
+++ src/test/java/com/next/bob/springboot/NextFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.bob.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
